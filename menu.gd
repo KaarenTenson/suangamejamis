49c129ec5279b798174e89345a6eea1226232457
KaarenTenson <tensonkaaren@gmail.com>
2024-01-26 22:28:15 +0200

    pani veits audiot sisse

--- menu.gd
+++ menu.gd
@@ -1,5 +1,6 @@
 extends Control
-
+@onready var audio_stream_player = $AudioStreamPlayer
+var kasonkiini = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,3 +14,11 @@ func _process(delta):
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://node_2d.tscn")
+
+
+func _on_volume_pressed():
+	if kasonkiini==false:
+		audio_stream_player.stop()
+		kasonkiini=true
+	else:
+		audio_stream_player.play()
