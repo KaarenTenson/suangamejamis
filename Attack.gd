a91dac4443262247d7554cc4d4992903700b6cf9
Gregor Artur <144439475+greeeegartur@users.noreply.github.com>
2024-01-27 13:03:29 +0200

    SFX assetitesse
    uued sfx

--- Attack.gd
+++ Attack.gd
@@ -1,28 +1,3 @@
-extends Node2D
-
-@onready var jyrki_sprite = $"../jyrki_sprite"
-@onready var attack_timer = $attack_timer
-@onready var attack_cooldown = $attack_cooldown
-@onready var telegraph_timer = $telegraph_timer
-
-signal attacking
-
-func start_attack():
-	attack_timer.start()
-		
-func _on_attack_timer_timeout():
-	if randf() < 0.5:
-		jyrki_sprite.play("telegraph")
-		telegraph_timer.start()
-	else:
-		attack_timer.start()
+extends "res://jyrki.gd"
 
 
-func _on_attack_cooldown_timeout():
-	attack_timer.start()
-
-
-func _on_telegraph_timer_timeout():
-	emit_signal("attacking")
-	jyrki_sprite.play("attack")
-	attack_cooldown.start()
