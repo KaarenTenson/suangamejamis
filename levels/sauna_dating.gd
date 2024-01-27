5565f2f660105a653525458c779e236fc3d0c663
Gregor Artur <144439475+greeeegartur@users.noreply.github.com>
2024-01-27 22:35:32 +0200

    ui pildid tagasi

--- levels/sauna_dating.gd
+++ levels/sauna_dating.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+const affection = 0
+
+var af = affection
+
 func _input(event: InputEvent):
 	# check if a dialog is already running
 	if Dialogic.current_timeline != null:
@@ -8,3 +12,21 @@ func _input(event: InputEvent):
 	if event is InputEventKey and event.keycode == KEY_ENTER and event.pressed:
 		Dialogic.start('saunadating_dialoge')
 		get_viewport().set_input_as_handled()
+
+func afplus10():
+	af += 10
+
+func afplus20():
+	af += 20
+	
+func afminus10():
+	af -= 10
+	
+func afminus20():
+	af -= 20
+
+func check():
+	if af < -30:
+		return
+	else:
+		pass
