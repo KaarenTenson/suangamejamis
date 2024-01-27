785eb099da7053dbae36ff4730bbd0dcb9247fa4
rauladam <raul.adamson@ut.ee>
2024-01-27 04:22:37 +0200

    Changed volume button to volume slider. Added background to next stage and added JYRKKI

--- menu.gd
+++ menu.gd
@@ -1,6 +1,4 @@
 extends Control
-@onready var audio_stream_player = $AudioStreamPlayer
-var kasonkiini = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,11 +12,3 @@ func _process(delta):
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://node_2d.tscn")
-
-
-func _on_volume_pressed():
-	if kasonkiini==false:
-		audio_stream_player.stop()
-		kasonkiini=true
-	else:
-		audio_stream_player.play()
