5d4e695a1e9458240453455fbd840c349eef5731
rauladam <raul.adamson@ut.ee>
2024-01-27 19:02:07 +0200

    Balanced Sauna Time and implemented progress bars

--- jyrki.gd
+++ jyrki.gd
@@ -3,17 +3,19 @@ extends Node2D
 @onready var _jyrki_sprite = $jyrki_sprite
 @onready var attack = get_node("jyrki_attack")
 
-const MAX_HEALTH = 100.0
+@onready var healthbars = %Healthbars
 
+
+const MAX_HEALTH = 100.0
 var hp = MAX_HEALTH
 
-
+func _process(delta):
+	healthbars.get_children()[1].get_children()[0].set_value_no_signal(hp)
 
 func _ready():
 	attack.start_attack()
 
 
 func _on_player_attacking():
-	if randf() < 0.5:
-		hp -= 20
-	print("JYRKI HP:" + str(hp))
+	if randf() < 0.65:
+		hp -= 15
--- player.gd
+++ player.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
-@onready var heal_cooldown = $heal_cooldown	
+@onready var heal_cooldown = $heal_cooldown
+@onready var attack_cooldown = $attack_cooldown
+@onready var duck_cooldown = $duck_cooldown
+
 @onready var healthbars = %Healthbars
 
 const HP_MAX = 100.0
@@ -13,26 +16,40 @@ var drunk = DRUNK_MIN
 
 signal attacking
 func _process(delta):
-	if Input.is_action_just_pressed("heal") && heal_cooldown.is_stopped:
+	
+	#Player progressbars
+	healthbars.get_children()[0].get_children()[1].set_value_no_signal(hp)
+	healthbars.get_children()[0].get_children()[2].set_value_no_signal(stamina)
+	healthbars.get_children()[0].get_children()[0].set_value_no_signal(drunk)
+	
+	#Player cooldowns
+	healthbars.get_children()[3].get_children()[1].set_value_no_signal((heal_cooldown.get_time_left()/2)*100)
+	healthbars.get_children()[2].get_children()[1].set_value_no_signal(attack_cooldown.get_time_left()*100)
+	healthbars.get_children()[4].get_children()[1].set_value_no_signal(duck_cooldown.get_time_left()*100)
+	
+	
+	if Input.is_action_just_pressed("heal") && heal_cooldown.is_stopped():
 		heal()
 		heal_cooldown.start()
 		
 		
 	if Input.is_action_pressed("duck") && stamina > 0:
-		stamina -= delta*25
-		print(stamina)
+		stamina -= delta*20
+		
+	if Input.is_action_just_released("duck"):
+		duck_cooldown.start()
 		
 	if stamina < 100:
-		stamina += delta*2.5
+		stamina += delta*5
 	
-	if Input.is_action_just_pressed("attack"):
+	if Input.is_action_just_pressed("attack") && attack_cooldown.is_stopped():
+		attack_cooldown.start()
 		attack()
-	healthbars.get_children()[0].get_children()[1].set_value_no_signal(hp)
 	
 func attack():
 	if stamina > 0:
 		emit_signal("attacking")
-		stamina -= 30.0
+		stamina -= 20.0
 	
 	
 func game_over():
@@ -40,7 +57,6 @@ func game_over():
 	
 func take_damage():
 	hp -= 20.0
-	print(hp)
 	if hp <= 0:
 		game_over()
 		
@@ -53,10 +69,11 @@ func _on_jyrki_attack_attacking():
 		take_damage()
 		
 func heal():
-	hp += 20.0;
+	hp += 20.0
+	stamina += 20.0
 	print("HP:"+str(hp))
-	if drunk < 50.0:
-		drunk += 30.0;
+	if drunk < 75.0:
+		drunk += 25.0;
 		print("DRUNK:" + str(drunk))
 	else:
 		game_over()
