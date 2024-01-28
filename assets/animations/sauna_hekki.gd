271c94de8423857f1cb339a1510c1418d626499e
rauladam <raul.adamson@ut.ee>
2024-01-28 15:23:46 +0200

    end screen

--- assets/animations/sauna_hekki.gd
+++ assets/animations/sauna_hekki.gd
@@ -3,6 +3,11 @@ extends AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$".".play("new_animation_2")# Called every frame. 'delta' is the elapsed time since the previous frame.
+	$".".play("new_animation_2")
+	
 func _process(delta):
 	pass
+
+
+func _on_animation_finished():
+	get_tree().change_scene_to_file("res://end_screens/the_end.tscn")
