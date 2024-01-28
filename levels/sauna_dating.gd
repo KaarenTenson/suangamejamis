9d92c27438a6df4d7f6f56edec6ee5c4e6818325
Gregor Artur <144439475+greeeegartur@users.noreply.github.com>
2024-01-28 06:34:48 +0200

    töötav dategame
    muusika, sfx ja affection system

--- levels/sauna_dating.gd
+++ levels/sauna_dating.gd
@@ -1,10 +1,5 @@
 extends Node2D
 
-const affection = 0
-
-var af = affection
-var minus30 = -30
-
 func _input(event: InputEvent):
 	# check if a dialog is already running
 	if Dialogic.current_timeline != null:
@@ -14,18 +9,8 @@ func _input(event: InputEvent):
 		Dialogic.start('saunadating_dialoge')
 		get_viewport().set_input_as_handled()
 
-func afplus10():
-	af += 10
-	print(af)
+func badend():
+	get_tree().quit()
 
-func afplus20():
-	af += 20
-	print(af)
-	
-func afminus10():
-	af -= 10
-	print(af)
-	
-func afminus20():
-	af -= 20
-	print(af)
+func _on_ready():
+	pass
