cf8ffd5afae4088780dc7e36c517e7eb58427a8c
andersaa1 <ander.saarniit@gmail.com>
2024-01-28 15:12:19 +0200

    animatsioon

---
+++ assets/animations/sauna_hekki.gd
@@ -0,0 +1,8 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$".".play("new_animation_2")# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
